6cf566c5914533e242bd43d256ee28ad9b331ace
Lucie Fabian <lucie.fabian@cpe.fr>
2022-12-05 14:01:14 +0100

    ok

--- Space Invader/graphsi_1.py
+++ Space Invader/graphsi_1.py
@@ -1,3 +1,11 @@
+"""
+Lucie Fabian
+Yamen Ben Guirat
+05/12/2022
+
+Programme permetttant de jouer à Space Invader à traver une fenêtre graphique
+"""
+
 from tkinter import Tk, Label, Button
 from tkinter import *
 
@@ -9,12 +17,11 @@ Title = Label(Fenetre, text = " Space Invaders",relief ='raised', fg = "blue", f
 Title.pack()
 BouttonQuitt = Button(Fenetre, text = " QUITER LA PARTIE", fg = "red",relief = 'groove', command =Fenetre.destroy )
 BouttonQuitt.pack(side = BOTTOM, padx = 30, pady = 70)
-Photo = PhotoImage(file = "jeu2.gif")
 
 Largeur = 2300
 Hauteur = 1000
 Canevas = Canvas(Fenetre, width = Largeur, height = Hauteur, bg = 'black')
-item = Canevas.create_image(650,200,anchor=NW,image=Photo)
+item = Canevas.create_image(650,200,anchor=NW)
 print("Image de fond(item",")")
 Canevas.pack()
 Fenetre.mainloop()
